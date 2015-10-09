b075ac68ef5c83dd5071850da53a7d4a74660d46
Dillon McGuire <DMMcGuire6724@gmail.com>
2015-10-09 11:01:33 -0400

    heroku setup

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express   = require('express'),
 	PORT      = process.env.PORT || 3000,
 	server    = express(),
-	MONOGOURI = process.env.MONGOLAB_URI,
+	MONOGOURI = process.env.MONGOLAB_URI || "mongodb://localhost:27017"
 	mongoose  = require('mongoose'),
 	dbname    = 'useful name';
 
 
-server.get('/', function (req, res) {
+server.get('/super-secret-test', function (req, res) {
 	res.write("Welcome to my app");
 	res.end()
 });
