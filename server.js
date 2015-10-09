1fc5348f7eb235e4c92a98c8d3aa6e5618c5a843
Dillon McGuire <DMMcGuire6724@gmail.com>
2015-10-09 10:40:21 -0400

    heroku uri

---
+++ server.js
@@ -0,0 +1,19 @@
+var express   = require('express'),
+	PORT      = process.env.PORT || 3000,
+	server    = express(),
+	MONOGOURI = process.env.MONGOLAB_URI,
+	mongoose  = require('mongoose'),
+	dbname    = 'useful name';
+
+
+server.get('/', function (req, res) {
+	res.write("Welcome to my app");
+	res.end()
+});
+
+
+mongoose.connect(MONOGOURI + '/' + dbname);
+
+server.listen(PORT, function() {
+	console.log("SERVER IS UP:", PORT)
+});
